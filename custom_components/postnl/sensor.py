6cfb8e1421a2f8b5d4793a07dc78217733bf845d
eelcob@hotmail.com <eelcob@hotmail.com>
2021-04-22 19:24:47 +0200

    debug

--- custom_components/postnl/sensor.py
+++ custom_components/postnl/sensor.py
@@ -18,6 +18,7 @@ _LOGGER = logging.getLogger(__name__)
 
 _LOGGER.warning("start Loading PostNL")
 
+
 def setup_platform(hass, config, add_entities, discovery_info=None):
     _LOGGER.debug("Setup PostNL sensor")
 
@@ -31,12 +32,13 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
             sensor_type = resource.lower()
 
             id = packagenumber
+            print (id)
         
-            #print('Type: '+ package['shipmentType'])
             type = package['shipmentType']
+            print (type)
 
-            print('State: '+ package['delivery']['status'])
             state = package['status']
+            print (state)
 
             if package['sender']:
                 if package['sender']['firstName']:
@@ -52,7 +54,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
                     sender = "uknown"
             else:
                 sender = "Unknown"
-            #print('Sender: '+ sender)
+            print (sender)
             package = PostNLSensor(id, sender, type, state)
 
             packagenumber = packagenumber + 1
@@ -86,7 +88,3 @@ class PostNLSenor(Entity):
     @property
     def state(self):
         return self._state
-
-    @property
-    def device_state_attributes(self):
-        return {ATTR_YEAR_MONTH_DAY_DATE: self._year_month_day_date, ATTR_LAST_UPDATE: self._last_update, ATTR_HIDDEN: self._hidden, ATTR_DAYS_UNTIL_COLLECTION_DATE: self._days_until_collection_date, ATTR_IS_COLLECTION_DATE_TODAY: self._is_collection_date_today}
